86a8283ae8bdd4cf707b67b69b9808cf6d013367
Данил Астахов <153730223+FixStress@users.noreply.github.com>
2024-03-22 14:51:32 +0300

    Вынес тест на исключение из класса с параметризацией в класс Lion

--- src/test/java/com/example/LionDoesHaveManeTests.java
+++ src/test/java/com/example/LionDoesHaveManeTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
 @RunWith(Parameterized.class)
 public class LionDoesHaveManeTests {
@@ -23,28 +22,20 @@ public class LionDoesHaveManeTests {
         return new Object[][]{
                 {"Самец", true},
                 {"Самка", false},
-                {"Некорректный ввод", false},
         };
     }
 
-    // Параметризированный тест метода doesHaveMane() с обработкой исключения
+    // Параметризированный позитивный тест метода doesHaveMane()
     @Test
     public void doesHaveManeTest() throws Exception {
-        if ("Самец".equals(sex) || "Самка".equals(sex)) {
-            Lion lion = new Lion(sex, feline);
-            boolean actualHasMane = lion.doesHaveMane();
-            assertEquals(expectedHasMane, actualHasMane);
-        } else {
-            String expectedExceptionMessage = "Используйте допустимые значения пола животного - самец или самка";
-            Exception actualException = assertThrows(Exception.class, () -> new Lion(sex, feline));
-            assertEquals(expectedExceptionMessage, actualException.getMessage());
-            // Добавил для информативности
-            System.out.println("При передаче некорректного значения: " + sex + "\nОтображается ошибка: " + actualException.getMessage());
-        }
+        Lion lion = new Lion(sex, feline);
+        boolean actualHasMane = lion.doesHaveMane();
+        assertEquals(expectedHasMane, actualHasMane);
+
     }
-
-
 }
+
+
 
 
 
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -10,6 +9,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -18,16 +18,16 @@ public class LionTest {
 
     @Mock
     Feline felineMock;
-    Lion lion;
-
-    @Before
-    public void setUp() throws Exception {
-        lion = new Lion("Самец", felineMock);
-    }
+    Feline feline;
 
     // Проверка вызова метода feline.getKittens() внутри метода lion.getKittens()
+    // У меня вопрос, мне вот кажется, что здесь можно использовать шпион льва И обойтись без мока feline
+    // чтобы убедиться, что вызван метод feline.getKittens() внутри метода lion.getKittens()
+    // Верно ли я рассуждаю? В следующем тесте мне кажется такая же ситуация
+    // Поправьте меня пожалуйста, если я не прав
     @Test
     public void getKittensTest() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
         lion.getKittens();
         verify(felineMock, Mockito.times(1)).getKittens();
     }
@@ -35,24 +35,35 @@ public class LionTest {
     // Проверка вызова метода feline.getFood("Хищник") внутри метода lion.getFood()
     @Test
     public void getFoodTest() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
         lion.getFood();
         verify(felineMock, Mockito.times(1)).getFood("Хищник");
     }
 
     // Проверка метода getKittens в классе lion через стаб
     @Test
-    public void getKittensTestWithStub() {
+    public void getKittensWithStubTest() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
         when(felineMock.getKittens()).thenReturn(1);
         assertEquals(felineMock.getKittens(), lion.getKittens());
     }
 
     // Проверка метода getFood в классе lion через стаб
     @Test
-    public void getFoodTestWithStub() throws Exception {
+    public void getFoodWithStubTest() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
         when(felineMock.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals(felineMock.getFood("Хищник"), lion.getFood());
 
 
+    }
+
+    // Тест на исключение при некорректном значении пола. Вынес из параметризации.
+    @Test
+    public void getExeptionTest() {
+        String expectedExceptionMessage = "Используйте допустимые значения пола животного - самец или самка";
+        Exception actualException = assertThrows(Exception.class, () -> new Lion("Некорректный ввод", feline));
+        assertEquals(expectedExceptionMessage, actualException.getMessage());
     }
 }
 
